1936d2dcbd2ea6b152a712f6e894d78d6b2184f2
Gabriel Covarrubias <gcovarrubias@c4-technologies.com>
2015-12-30 17:48:18 -0800

    Added validation to __construct,  update_fields and call_api_url with Exceptions

--- vicidialAgentAPI.php
+++ vicidialAgentAPI.php
@@ -26,8 +26,18 @@ class VicidialAgentAPI {
 
 	/**
 	 * Initializes the object with the basic information to create the base_url
+	 *
+	 * TODO: Unable to test server_ip validation with hostname instead of ip address		
 	 */
 	public function __construct($server_ip, $source, $api_user, $api_password, $debug = false){
+		
+		// Validates if valid IP or resolv hostname WARNING: Not fully tested !!
+		if ((filter_var($server_ip, FILTER_VALIDATE_IP) === false) && (filter_var(gethostbyname($server_ip), FILTER_VALIDATE_IP) === false)) {
+
+			throw new Exception('Invalid IP Address or hostname not found');
+                        return false;
+
+		}
 
 		$this->server_ip = $server_ip;
 		$this->source = $source;
@@ -60,11 +70,22 @@ class VicidialAgentAPI {
                 curl_setopt_array( $curl, array (
                         CURLOPT_URL => $url,
                         CURLOPT_HEADER => false,
+                        CURLOPT_CONNECTTIMEOUT => 5,
+                        CURLOPT_FAILONERROR => true,
                         CURLOPT_RETURNTRANSFER => true )
                 );
 
 		// Store the response from the Web method
                 $response = curl_exec($curl);
+
+		$curl_error =  curl_error($curl);
+
+		if($curl_error)
+		{
+    			throw new Exception("cURL $curl_error");
+                        return false;
+		}
+
                 curl_close($curl);
 
                 return $response;
@@ -214,6 +235,29 @@ class VicidialAgentAPI {
 
         public function update_fields($agent_user, $fields_to_update)
         {
+
+		
+		if ( !is_array($fields_to_update)) {
+
+			throw new Exception('Fields must be an array');
+			return false;
+		}
+
+		// according to the API documentation only these fields are allowed to update using this method
+		$permited_fields = array("address1","address2","address3","rank","owner","vendor_lead_code",
+					"alt_phone","city","comments","country_code","date_of_birth","email","first_name",
+					"gender","gmt_offset_now","last_name","middle_initial","phone_number","phone_code",
+					"postal_code","province","security_phrase","source_id","state","title"
+				   );
+	
+		foreach( $fields_to_update as $key => $value ){
+			if ( !in_array($key, $permited_fields)) {
+
+				throw new Exception("$key is not a valid field");
+                        	return false;
+			}				
+		}
+
                 $url_parameters['function'] = "update_fields";
                 $url_parameters['agent_user'] = $agent_user;
 
